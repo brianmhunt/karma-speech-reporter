5c618044fd4bbcaaedd2b43d6839dcb83b84689c
Brian M Hunt <brianmhunt@gmail.com>
2015-04-16 14:45:59 -0400

    Birthed

---
+++ README.md
@@ -0,0 +1,31 @@
+Karma Speech Reporter
+===
+
+A third party [Karma](http://karma-runner.github.io/) reporter for speaking results.
+
+Installation
+---
+
+Add it as a `devDependency` to package json. e.g.
+
+```bash
+npm install karma-speech-reporter --save-dev
+```
+
+
+Configuration
+---
+
+Add it to the list of reporters.
+
+
+Change Log
+---
+
+- 1.0.0 Birthed.
+
+
+License
+---
+
+MIT.
---
+++ index.js
@@ -0,0 +1,19 @@
+var say = require('say')
+
+var SpeechReporter = function(baseReporterDecorator) {
+	baseReporterDecorator(this);
+
+	this.onSpecComplete = this.writeCommonMsg = function() { };
+
+  this.onRunComplete = function(browsers, results) {
+		if (results.failed || results.error) {
+			say("" + results.failed + " tests failed, " + results.error + " test errors.")
+		}
+	};
+};
+
+SpeechReporter.$inject = ['baseReporterDecorator'];
+
+module.exports = {
+	'reporter:speech': ['type', SpeechReporter]
+};
